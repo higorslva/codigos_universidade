eafbde6464f81e2c7ed2ee8a7a6ecf9c22011fcb
Higor Silva <higors88@gmail.com>
2023-02-01 15:34:39 -0300

    Update

--- R/Prob. e Estatistica/Aula 1/Aula1.R
+++ R/Prob. e Estatistica/Aula 1/Aula1.R
@@ -124,3 +124,62 @@ gapminder
 data(gapminder)
 head(gapminder)
 names(gapminder)
+#-----------------------Aula 01/fev-----------------------#
+
+library(tidyverse)
+gapminder <- readRDS('/home/higorslva/Documentos/Unifap/C. Computação/Semestres/2022.2/Probabilidade e Estatística/Materiais/gapminder_dados.rds')
+gapminder
+
+names(gapminder)
+str(gapminder)
+levels(gapminder$pais)
+
+# Os 4 continentes são listados com levels()
+a <- levels(gapminder$continente)
+cat('Continentes: \n')
+for(k in 1:4){cat(a[k], '\n') }
+
+# As 8 regiões são listados com levels()
+b <- levels(gapminder$região)
+cat('Regiões: \n')
+for(k in 1:8){cat(b[k], '\n') }
+
+# O intervalos de valores da variável ano é calculado com range()
+c <- range(gapminder$ano)
+cat('menor ano = ',c[1], '\n')
+cat('maior ano = ',c[2], '\n')
+
+# O intervalos de valores da variável renda per capita é calculado com range()
+d<- range(gapminder$renda_per_capita)
+cat('menor renda per capita = ',d[1], '\n')
+cat('maior renda percapita = ',d[2], '\n')
+
+## Tem dados faltantes, para omitir estes dados usar a opção na.rm=TRUE
+d <- range(gapminder$renda_per_capita, na.rm = TRUE)
+cat('menor renda per capita = ',d[1], '\n')
+cat('maior renda percapita = ',d[2], '\n')
+
+# Os 4 niveis de nivel de renda são listados com levels()
+e <- levels(gapminder$nivel_de_renda)
+cat('Niveis de renda: \n')
+for(k in 1:4){cat(e[k], '\n')}
+
+# O intervalos de valores da variável mortalidade infantil é calculado com range()
+f <- range(gapminder$mortalidade_infantil)
+cat('menor taxa de mortalidade ',f[1], '\n')
+cat('maior taxa de mortalidade = ',f[2], '\n')
+
+## Tem dados faltantes, para omitir estes dados usar a opção na.rm=TRUE
+f <- range(gapminder$mortalidade_infantil, na.rm = TRUE)
+cat('menor taxa de mortalidade ',f[1], '\n')
+cat('maior taxa de mortalidade = ',f[2], '\n')
+
+# O intervalos de valores da variável expectativa de vida é calculado com range()
+g <- range(gapminder$expectativa_vida)
+cat('menor expectativa de vida ',g[1], '\n')
+cat('maior expectativa de vida = ',g[2], '\n')
+
+## Tem dados faltantes, para omitir estes dados usar a opção na.rm=TRUE
+g <- range(gapminder$expectativa_vida, na.rm = TRUE)
+cat('menor expectativa de vida ',g[1], '\n')
+cat('maior expectativa de vida = ',g[2], '\n')
