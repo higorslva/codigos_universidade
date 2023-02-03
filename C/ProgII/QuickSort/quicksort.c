224abf72f60897da3e931113e30766f85201d4c7
Higor Silva <higors88@gmail.com>
2023-02-03 10:30:13 -0300

    Sync

--- C/ProgII/QuickSort/quicksort.c
+++ C/ProgII/QuickSort/quicksort.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//#define MAX 10
-
-//Protótipo da função:
-void quicksort (int *a, int esquerdo, int direito){
-  int i, j, x, y;
-  i = esquerdo;
-  i = direito;
-  x = a[(esquerdo + direito)/2];
-
-  while(i <= j){
-    while(a[i] < x && i < direito){
-      i++;
+ 
+void quicksort(int *a, int esq, int dir) {
+    int i, j, x, y;
+     
+    i = esq;
+    j = dir;
+    x = a[(esq + dir) / 2];
+     
+    while(i <= j) {
+        while(a[i] < x && i < dir) {
+            i++;
+        }
+        while(a[j] > x && j > esq) {
+            j--;
+        }
+        if(i <= j) {
+            y = a[i];
+            a[i] = a[j];
+            a[j] = y;
+            i++;
+            j--;
+        }
     }
-    while(a[j] > x && j > esquerdo){
-      j--;
+     
+    if(j > esq) {
+        quicksort(a, esq, j);
     }
-    if(i <= j){
-      y = a[i];
-      a[i] = a[j];
-      a[j] = y;
-      i++;
-      j--;
+    if(i < dir) {
+        quicksort(a, i, dir);
     }
-  }
-  if (j > esquerdo){
-    quicksort(a, esquerdo, j);
-  }
-  if(i < direito){
-    quicksort(a, i, direito);
-  }
 }
 
-int main(){
-  int i, vet[10];
-  for (i = 0; i < 10; i++){
-    printf("Digite um valor: ");
-    scanf("%d", &vet[i]);
-  }
-  quicksort(vet, 0, 10 - 1);
-
-  printf("Valores ordenados: \n");
-  for(i = 0; i < 10; i++){
-    printf("%d", vet[i]);
-  }
-  //system("pause");
-  return 0;
+ 
+int main(int argc, char** argv){
+    int i;//, vet[10];
+    int vet[10] = {3, 6, 5, 2, 9, 8, 1, 4, 7, 0};
+    printf("Valores desordenados: \n|");
+    for (i = 0; i < 10; i++){
+        printf("%d|", vet[i]);
+    }
+    quicksort(vet, 0, 10 - 1);
+ 
+    printf("\n\nValores ordenados: \n|");
+    for(i = 0; i < 10; i++){
+        printf("%d|", vet[i]);
+    }
+    return 0;
 }
